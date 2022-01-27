9213ab09666457db0ebe27a6657c5264597c9b70
Dennis <tougent@gmail.com>
2022-01-27 14:14:06 +0100

    feat: changed deprecated keykodes for keys

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,11 +16,10 @@ $(function () {
     //keys
     $(document).keydown(function (event) {
 
-        let key = event.keyCode;
-        //console.log(key);
+        let key = event.key;
+        console.log(event);
         switch (key) {
-            case 13:
-                //enter
+            case 'Enter':
                 event.preventDefault();
                 if (btn.reset) {
                     return;
@@ -32,97 +31,92 @@ $(function () {
                 screen.val(result);
                 btn.reset = true;
                 break;
-            case 67:
-                //c
+            case 'c' || 'C':
                 event.preventDefault();
                 btn.clear();
                 break;
-            case 191:
+            case '*':
                 event.preventDefault();
                 btn.resetIt('*');
                 btn.calcBtn('*');
                 break;
-            case 187:
+            case '+':
                 event.preventDefault();
                 btn.resetIt('+');
                 btn.calcBtn('+');
                 break;
-            case 189:
+            case '-':
                 event.preventDefault();
                 btn.resetIt('-');
                 btn.calcBtn('-');
                 break;
-            case 190:
+            case '.':
                 event.preventDefault();
                 btn.resetIt('.');
                 btn.calcBtn('.');
                 break;
-            case 47:
+            case '/':
                 event.preventDefault();
                 btn.resetIt('/');
                 btn.calcBtn('/');
                 break;
-            case 48:
+            case '0':
                 event.preventDefault();
                 btn.resetIt(0);
                 btn.calcBtn(0);
                 break;
-            case 49:
+            case '1':
                 event.preventDefault();
                 btn.resetIt(1);
                 btn.calcBtn(1);
                 break;
-            case 50:
+            case '2':
                 event.preventDefault();
                 btn.resetIt(2);
                 btn.calcBtn(2);
                 break;
-            case 51:
+            case '3':
                 event.preventDefault();
                 btn.resetIt(3);
                 btn.calcBtn(3);
                 break;
-            case 52:
+            case '4':
                 event.preventDefault();
                 btn.resetIt(4);
                 btn.calcBtn(4);
                 break;
-            case 53:
+            case '5':
                 event.preventDefault();
                 btn.resetIt(5);
                 btn.calcBtn(5);
                 break;
-            case 54:
+            case '6':
                 event.preventDefault();
                 btn.resetIt(6);
                 btn.calcBtn(6);
                 break;
-            case 55:
+            case '7':
                 event.preventDefault();
                 btn.resetIt(7);
                 btn.calcBtn(7);
                 break;
-            case 56:
+            case '8':
                 event.preventDefault();
                 btn.resetIt(8);
                 btn.calcBtn(8);
                 break;
-            case 57:
+            case '9':
                 event.preventDefault();
                 btn.resetIt(9);
                 btn.calcBtn(9);
                 break;
             default:
             return (
-                key == 8 || 
-                key == 9 ||
-                key == 13 ||
-                key == 46 ||
-                key == 110 ||
-                key == 190 ||
-                (key >= 35 && key <= 40) ||
-                (key >= 48 && key <= 57) ||
-                (key >= 96 && key <= 105));
+                key === 'Backspace' || 
+                key === 'ArrowLeft' ||
+                key === 'ArrowRight' ||
+                key === 'Delete'
+            );
             break;
 
         }
